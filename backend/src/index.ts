a037c650889ad7c527aefefd67cc8134e197d751
Frans Jesky <jesky.yeremi@gmail.com>
2026-01-10 20:31:13 +0700

    feat: setup boilerplate for frontend and add prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import routes from "./routes/index.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
+
 app.use("/api", routes);
 
-app.listen(3001, () => console.log("listening on port 3001"));
+app.listen(process.env.PORT, () => console.log(`server is up and listening on http://localhost:${process.env.PORT}`));
